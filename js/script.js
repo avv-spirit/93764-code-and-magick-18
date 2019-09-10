45345dbe3eb89ca8f77860decf2453f9d2529afb
Vladimir <Sp1rit911@mail.ru>
2019-09-11 00:28:18 +0300

    Работаю над первой частью задания

---
+++ js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var fireballSize = 22;
+var wizardSpeed = 3;
+var wizardWidth = 70;
+
+var getFireballSpeed = function (left) {
+  if (left === true) {
+    return 5;
+  } else {
+    return 2;
+  }
+};
+getFireballSpeed();
+
+var getWizardHeight = function () {
+  var wizardHeight = 1.337 * wizardWidth;
+  return getWizardHeight;
+};
+getWizardHeight();
